5ee15dd1b9f03e0bac72318b735128c2156d83aa
clementigbinoba <108894960+clementigbinoba@users.noreply.github.com>
2023-04-23 20:19:24 +0100

    100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * swap_ints - Switch multiple integers in array.
+ * @a: first integer Switch.
+ * @b: second integer Switch.
  */
 void swap_ints(int *a, int *b)
 {
@@ -15,12 +15,12 @@ void swap_ints(int *a, int *b)
 }
 
 /**
- * shell_sort - Sort an array of integers in ascending
- *              order using the shell sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
+ * shell_sort - array integers sorted in ascending pattern
+ *              Through shell sort algorithm.
+ * @array: array of integers.
+ * @size: size of the array.
  *
- * Description: Uses the Knuth interval sequence.
+ * Description: application of Knuth interval sequence.
  */
 void shell_sort(int *array, size_t size)
 {
