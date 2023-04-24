994460c83959fca16dbbfb9b657cacc2763d559a
clementigbinoba <108894960+clementigbinoba@users.noreply.github.com>
2023-04-24 04:18:46 -0700

    print_array.c

--- print_array.c
+++ print_array.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints an array of integers
+ * print_array - Prints integers array
  *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * @array: array to  print
+ * @size: elements in array
  */
 void print_array(const int *array, size_t size)
 {
