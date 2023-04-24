8915fd0bd76ae5241c70f4ea7e34963f062a93e7
clementigbinoba <108894960+clementigbinoba@users.noreply.github.com>
2023-04-24 04:15:59 -0700

    deck.h

--- deck.h
+++ deck.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 /**
- * enum kind_e - Enumeration of card suits.
+ * enum kind_e - listing of card suits.
  * @SPADE: Spades suit.
  * @HEART: Hearts suit.
  * @CLUB: Clubs suit.
@@ -21,7 +21,7 @@ typedef enum kind_e
 /**
  * struct card_s - Playing card
  *
- * @value: Value of the card
+ * @value: Value of  card
  * From "Ace" to "King"
  * @kind: Kind of the card
  */
